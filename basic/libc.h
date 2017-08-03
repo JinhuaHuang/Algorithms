7e9354130affe1e1080557166afe52b4ec6b3f74
JinhuaHuang <jasonstatham1972@sina.com>
2017-08-03 17:08:29 +0800

    Update libc.h

--- basic/libc.h
+++ basic/libc.h
@@ -1,7 +1,13 @@
-
 #pragma once
 
-#define STRLEN strlen
+#define basic_strlen strlen
 
 //string
-int strlen(const char *s);
+int strlen(char const *s);
+
+char* strcpy(char *dst, char const *src);
+char* strncpy(char *dst, char const *src, int n);
+
+int strcmp(char const *s1, char const *s2);
+int strncmp(char const *s1, char const *s2, int n);
+
